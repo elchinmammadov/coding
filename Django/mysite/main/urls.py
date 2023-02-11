0f9ecc7ff77ecba9250c42d48cecaee3b4335e19
elchinmammadov <l4n_mammadov@yahoo.com>
2023-02-11 14:32:19 +0000

    django project

--- Django/mysite/main/urls.py
+++ Django/mysite/main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 path('<int:id>', views.index, name='index'),
 # path('<str:name>', views.index, name='index'),
 path('', views.home, name='home'),
+path('create/', views.create, name='create'),
 # path('', views.index, name='index'),
 # path('v1/', views.v1, name='view 1'),
 ]
- 
--- Django/mysite/main/views.py
+++ Django/mysite/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
-
+from .forms import CreateNewList
 
 # Create your views here.
 def index(response, id):
@@ -14,7 +14,19 @@ def index(response, id):
     return render(response, 'main/list.html', {"ls": ls})
 
 def home(response):
-    return render(response, 'main/home.html', {})
+    return render(response, 'main/home.html', {}) # home page (i.e. landing page)
+
+def create(response):
+    if response.method == 'POST':
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
+            return HttpResponseRedirect('/%i' % t.id)
+    else:
+        form = CreateNewList()
+    return render(response, 'main/create.html', {'form': form}) # create to-do list
 
 # def index(response):
 #     return HttpResponse('<h1>Hello world</h1>')
