eaa108c6dd5ef5eea313f05a4d26e5b56d6df470
elchinmammadov <l4n_mammadov@yahoo.com>
2023-02-12 23:03:47 +0000

    added 'clear' button

--- GUI/citations_tool_GUI.py
+++ GUI/citations_tool_GUI.py
@@ -2,6 +2,15 @@ import customtkinter as ctk # to build GUI
 import pyperclip as pc # to copy formatted citation to clipboard
 import re # to split string using multiple delimiters
 
+#You can use these button options in ctk:
+    # label: CTkLabel
+    # button: CTkButton
+    # entry field: CTkEntry
+    # combo box: CTkOptionMenu
+    # check box: CTkCheckBox
+    # radio button: CTkRadioButton
+    # text box: CTkTextbox
+
 dict = {} # create an empty dictionary to store and manipulate user inputs (author, title, etc.)
 
 # set GUI color settings
@@ -39,6 +48,8 @@ daygui = ctk.CTkEntry(master=frame, placeholder_text="Day (optional)")
 daygui.pack(pady=3, padx=3)
 checkbox = ctk.CTkCheckBox(master=frame, text="All authors in one line", onvalue="Yes", offvalue="No")
 checkbox.pack(pady=3, padx=3)
+textbox = ctk.CTkTextbox(master=frame, height=40, width=200, corner_radius=0, state="disabled")
+textbox.pack(pady=3, padx=3)
 
 # function to check if all required fields are filled in
 def validation_func(dict):
@@ -47,6 +58,7 @@ def validation_func(dict):
             if dict[x] == "":
                 print("Please fill in all required fields.")
                 dict[x] = str(input("Please enter " + x + ": "))
+    #TODO add code to check if authors have both name and surname are filled in using split(" ") and using len() on the result to see if there are at least 2 items in that list
     return dict
 
 # function to change month number to month name
@@ -117,27 +129,21 @@ def month_func(dict):
         dict['month_name'] = "error"
     return dict
 
-# when users input multiple names into 'author1' field, this funciton will split them into individual authors, then save result into dictionary under dict['author']
-def multi_author_func(dict):
-    if dict['multi_authors'] == "Yes":
+# function to loop through all authors, save them as surname followed by first name, then save to dict dictionary as 'author' key
+def author_func(dict):
+    if dict['multi_authors'] == "Yes": # when users input multiple names into 'author1' field, this will split them into individual authors, then save result into dictionary under dict['author']
         authors = re.split(r', and | and | & | , |, |,', str(dict['author1'])) # splits names using multiple delimiters (i.e. ' and ' ' & ' ' , ' etc)
         author = ""
         # swaps around names and surnames, then saves result into dictionary under dict['author']
         for x in authors:
             author = author + str(x).split(" ")[1] + ", " + str(x).split(" ")[0] + ", "
-        dict['author'] = author[:-2] # remove last 2 characters from the 'author' string, then save it in 'dict' dictionary under 'author' key 
-    return dict
-
-# function to loop through all authors, same them as surname followed by first name, then save to dict dictionary as 'author' key
-def author_func(dict):
-    if dict['multi_authors'] == "Yes": # if multiple authors are entered in dict['author1'], then launches multi_author_func to parse those authors
-        dict = multi_author_func(dict)
-    else: # if a single author is entered in dict['author1'], then loops through all authors
+    else: # if a single author is entered in dict['author1'], then loops through all authors in author1, author2, author3 and author4
         author = ""
+        # swaps around names and surnames, then saves result into dictionary under dict['author']
         for x in ['author1', 'author2', 'author3', 'author4']:
             if dict[x] != "":
                 author = author + str(dict[x]).split(" ")[1] + ", " + str(dict[x]).split(" ")[0] + ", "
-        dict['author'] = author[:-2] # remove last 2 characters from the 'author' string, then save it in 'dict' dictionary under 'author' key 
+    dict['author'] = author[:-2] # remove last 2 characters from the 'author' string, then save it in 'dict' dictionary under 'author' key 
     return dict
 
 # function to combine all user inputs and return it as a citation under 'combined_list' key in 'dict' dictionary
@@ -159,6 +165,21 @@ def combine_func(dict):
 def copy_to_clipboard(dict):
     pc.copy(dict['combined_list'])
 
+def clear(dict):
+    author1gui.delete(0, 'end')
+    author2gui.delete(0, 'end')
+    author3gui.delete(0, 'end')
+    author4gui.delete(0, 'end')
+    titlegui.delete(0, 'end')
+    organisationgui.delete(0, 'end')
+    yeargui.delete(0, 'end')
+    monthgui.delete(0, 'end')
+    daygui.delete(0, 'end')
+    checkbox.deselect()
+    textbox.delete(0, 'end')
+    dict = {}
+    return dict
+
 def login(dict):
     # save values inputted by user as variables
     dict['author1'] = author1gui.get()
@@ -171,21 +192,26 @@ def login(dict):
     dict['month'] = str(monthgui.get())
     dict['day'] = str(daygui.get())
     dict['multi_authors'] = str(checkbox.get())
+
     # run functions
     dict = validation_func(dict) # checks if all required fields are filled in
     dict = month_func(dict) # adds letter name of the month to the dictionary
     dict = author_func(dict) # combines all authors into a single string with surname followed by first name
     dict = combine_func(dict) # combines all user inputs into a single string to show as a citation
     copy_to_clipboard(dict) # copies data from dict['combined_list'] to clipboard
+
     # output final result
-    output = ctk.CTkLabel(master=frame, text=dict['combined_list'], font=("Roboto", 10))
-    output.pack(pady=4, padx=3)
-    output = ctk.CTkLabel(master=frame, text='Your formatted citation has been copied to clipboard.', font=("Roboto", 10))
-    output.pack(pady=1, padx=3)
+    textbox.delete("0.0", "200.0")
+    textbox.insert("0.0", text=str(dict['combined_list'])) # BUG I get no error but this text box doesn't show output data
+    textbox.pack(pady=4, padx=3)
+    output_msg = ctk.CTkLabel(master=frame, text='Your formatted citation has been copied to clipboard.', font=("Roboto", 10))
+    output_msg.pack(pady=1, padx=3)
+    output_text = ctk.CTkLabel(master=frame, text=dict['combined_list'], font=("Roboto", 10))
+    output_text.pack(pady=4, padx=3)
 
 submit_button = ctk.CTkButton(master=frame, text="Submit", command=lambda: login(dict)) # Submit button
 submit_button.pack(pady=3, padx=3)
-clear_button = ctk.CTkButton(master=frame, text="Clear", command=lambda: login(dict)) # Clear button
+clear_button = ctk.CTkButton(master=frame, text="Clear", command=lambda: clear(dict)) # Clear button
 clear_button.pack(pady=3, padx=3)
 
 
